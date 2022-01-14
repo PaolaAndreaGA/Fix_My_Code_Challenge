1733ae8093fdf56f4e83d279065078c3c3b27409
PaolaAndreaGA <paga0209@gmial.com>
2022-01-13 21:32:29 -0500

    1 square

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -18,7 +18,7 @@ size = parseInt(process.argv[2], 10)
 
 for (let i = 0 ; i < size ; i ++) {
     for (let j = 0 ; j < size ; j ++) {
-        process.stdout.write("#");
+	process.stdout.write("#");
     }
     process.stdout.write("\n");
 }
